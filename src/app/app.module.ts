8739f2f38f3a3f57f088fea00aa624b0d504a118
Fayaazuddin Ali Mohammad <a.fayaaz@gmail.com>
2020-02-16 22:16:24 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+import {FormsModule} from "@angular/forms";
+import {HeaderComponent} from "./header/header.component";
+import {RecipesComponent} from './recipes/recipes.component';
+import {RecipesListComponent} from './recipes/recipes-list/recipes-list.component';
+import {RecipesDetailsComponent} from './recipes/recipes-details/recipes-details.component';
+import {RecipesItemComponent} from './recipes/recipes-list/recipes-item/recipes-item.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        RecipesComponent,
+        RecipesListComponent,
+        RecipesDetailsComponent,
+        RecipesItemComponent,
+        ShoppingListComponent,
+        ShoppingEditComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,HeaderComponent,RecipesComponent,
+    RecipesListComponent,RecipesDetailsComponent,
+    RecipesItemComponent,ShoppingEditComponent,
+    ShoppingListComponent]
 })
 export class AppModule { }
